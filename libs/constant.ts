c9dae33b07308f6569e37b60a3acce3d7ae8ee93
maluda <nwitevictor7@gmail.com>
2025-03-27 20:20:32 +0100

    updated app name, slogan and description

---
+++ libs/constant.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = process.env.APP_NAME || "Real Estate DApp";
+export const APP_SLOGAN =
+  process.env.APP_SLOGAN ||
+  "Revolutionizing Real Estate with Blockchain Transparency";
+export const APP_DESCRIPTION =
+  process.env.APP_DESCRIPTION ||
+  "Real Estate DApp is a cutting-edge decentralized application built on the Ethereum blockchain. It leverages the power of Hardhat for smart contract development, Next.js for a seamless front-end experience, and MongoDB for efficient data management. By harnessing blockchain technology, this platform offers transparent, secure, and efficient real estate transactions, bringing the future of property buying and selling to your fingertips.";
